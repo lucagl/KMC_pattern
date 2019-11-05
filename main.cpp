dc5e5f40949cee4c7ff4bb3b9600cd824e23ba34
Luca Gagliardi <luca.gagliardi@iit.it>
2019-11-05 11:23:10 +0100

    Merged new main of master branch.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
  ------------ COMPILING INSTRUCTIONS ----------------
 place in working folder *.cpp and *.h
-compile: mpic++ -openmp *.cpp -o "executable".ex 
+compile: mpic++ *.cpp -o "executable".ex 
 run:  mpirun -np "cores number" executable.out
 input file: file Input.txt containing input informations must be present 
 
@@ -58,10 +58,9 @@ TEMPLATE: to make function or classes versatile on different types
 #include "kmc.h"
 #include "functions.h"
 
-
 // --- PARALLELISATION
-#include "thread"
 #include <mpi.h>
+#include <ctime>
 //------
 
 
@@ -71,85 +70,89 @@ const int root_process=0;
 int proc_ID;
 int ierr,n_proc;
 	
-unsigned total_n_proc = std::thread::hardware_concurrency();
-
-
 int main(int argc, char **argv){
 	
 
-//	const double  J=1, A= 0.1, F = 0.025;//Maybe some from input file?
+clock_t t1,t2;
+t1=clock();
+float seconds;
+
+long elapsed_time;
 
 
+std :: time_t start,end, curr_time;
+start = std::time(NULL);
 	
-
-	//int x,y; //just dummy variables fr clarity
-	
-
-	
-
-	clock_t t1,t2;
-	t1=clock();
-	float seconds;
-	
-
-	
-	ierr = MPI_Init(&argc, &argv);
-
-	MPI_Comm_size(MPI_COMM_WORLD, &n_proc);
-
-	MPI_Comm_rank(MPI_COMM_WORLD, &proc_ID);
 	
 
 
-	const double J =0.2;
+ierr = MPI_Init(&argc, &argv);
 
-	double T0,conc0, A, BR;
-	int L,radius, n_steps;
-	int frame, print_every;
+MPI_Comm_size(MPI_COMM_WORLD, &n_proc);
+
+MPI_Comm_rank(MPI_COMM_WORLD, &proc_ID);
+
+
+
+const double J =0.2;
+
+double T0,conc0, A, BR, c_eq;
+int L, radius, n_steps;
+int frame, print_every;
+
+bool read_old,is_circle;
 	
-	bool read_old,is_circle;
-	
-	if(proc_ID == root_process){
-		// read from input file and broadcast
-		std :: cout << "\n Total number of cores available= "<< total_n_proc << std :: endl ;	
-		std :: cout << "\n Number of parallel simulations launched = "<< n_proc << std :: endl ;
 
-		read_input(&L, &T0,& conc0, &radius,&is_circle, &A, &BR, &n_steps, &print_every, &read_old);
+// ----------------- READ INPUT AND PRINT INITIAL INFO -------------
+if(proc_ID == root_process){
+	// read from input file and broadcast
+	time(&curr_time);
+	std :: cout << "\n Start time " << ctime(&curr_time) << "\n";	
+	std :: cout << "\n Number of processors= "<< n_proc << std :: endl ;
 
-		std :: cout << "\n J= " << J << "  |  L= "<< L<< "  |  T =" << T0 <<"  |  concentration = "<< conc0 <<
-		 "  |  initial island radius = "<< radius <<  "  |  'attachment parameter ' =" << A << "  |  Bond energy ratio = "<< BR <<"  |  kmc steps =" << n_steps<<
-		"  |  print each =" << print_every << "  |  read old file? =" << read_old<<"\n";
-	}
+	read_input(&L, &T0,& conc0, &radius,&is_circle, &A, &BR, &n_steps, &print_every, &read_old);
 
-	//RootID broadcast data to other processors
-	MPI_Bcast(&L, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&T0, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&conc0, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&radius, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&is_circle, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&BR, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&A, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&n_steps, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&print_every, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&read_old, 1, MPI_C_BOOL, 0, MPI_COMM_WORLD);
+	std :: cout << "\n J= " << J << "  |  L= "<< L<< "  |  T =" << T0 <<"  |  concentration = "<< conc0 <<
+		"  |  initial island radius = "<< radius <<  "  |  'attachment parameter ' =" << A << "  |  Bond energy ratio = "<< BR <<"  |  kmc steps =" << n_steps<<
+	"  |  print each =" << print_every << "  |  read old file? =" << read_old<<"\n";
 
-	// make directories for each thread
+	c_eq = exp((-2*J*(1+BR))/T0);
 
-	auto aString = "mkdir plots" + (std::to_string(proc_ID));
-	const char* makeDir = aString.c_str();
-	auto path = "plots" + (std::to_string(proc_ID));
-	auto bString ="rm " + path + "/island* " + path + "/adatom* ";
-	const char* remove_old = bString.c_str();
-	system(makeDir);
-	system(remove_old);
+
+	std :: cout << "\n Equilibrium concentration at T = 0" << c_eq << "\n";
+}
+
+//RootID broadcast data to other processors
+MPI_Bcast(&L, 1, MPI_INT, 0, MPI_COMM_WORLD);
+MPI_Bcast(&T0, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+MPI_Bcast(&conc0, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+MPI_Bcast(&radius, 1, MPI_INT, 0, MPI_COMM_WORLD);
+MPI_Bcast(&is_circle, 1, MPI_INT, 0, MPI_COMM_WORLD);
+MPI_Bcast(&BR, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+MPI_Bcast(&A, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+MPI_Bcast(&n_steps, 1, MPI_INT, 0, MPI_COMM_WORLD);
+MPI_Bcast(&print_every, 1, MPI_INT, 0, MPI_COMM_WORLD);
+MPI_Bcast(&read_old, 1, MPI_C_BOOL, 0, MPI_COMM_WORLD);
+
+// make directories for each thread
+
+auto aString = "mkdir plots" + (std::to_string(proc_ID));
+const char* makeDir = aString.c_str();
+auto path = "plots" + (std::to_string(proc_ID));
+auto bString ="rm " + path + "/island* " + path + "/adatom* ";
+const char* remove_old = bString.c_str();
+system(makeDir);
+system(remove_old);
+
+// -------------------------------------------------------
 
 
 // 	___________________INITIALIZATION _____________________________
 
-	srand (time(NULL)*(proc_ID+1));// initialise random generator differently for each thread
-	KMC kmc(J,BR,A);
-	kmc.init(L,is_circle,radius,conc0,T0, read_old);
-	//kmc.print(0);
+srand (time(NULL)*(proc_ID+1));// initialise random generator differently for each thread
+KMC kmc(J,BR,A);
+kmc.init(L,is_circle,radius,conc0,T0, read_old);
+kmc.print(0);
 
 
 // _________________________RUN KMC ___________________________
@@ -157,14 +160,17 @@ int main(int argc, char **argv){
 
 frame = 0;
 
-for (int k = 0; k < n_steps; k++){
-	// Temperature function..
+for (int k = 1; k <= n_steps; k++){
 
+	/* Temperature can be changed here..
+	T = T0 + ..
+	*/
+	// EVOLUTION STEP 
 	kmc.step(T0,false);
 
 	if ((k%print_every)== 0){
-		kmc.print(frame);
 		frame+=1;
+		kmc.print(frame);
 	}
 	if(k%(n_steps/10)==0&&proc_ID == root_process){
 		std :: cout  << " | "<< std :: flush;
@@ -190,10 +196,13 @@ if(proc_ID==0){
 
 	std :: cout << "\n Numeber of parallel KMCs ="<< n_proc<<"\n";
 	
-		
+	end = std :: time(NULL);
+	elapsed_time = end-start;
 	t2 = clock();
 	seconds = ((float)t2-(float)t1)/ CLOCKS_PER_SEC;
-	std :: cout << "\n Elapsed time = " << seconds <<"s \n"<< std:: endl;
+	std :: cout << "\n Elapsed CPU time = " << seconds <<"s \n"<< std:: endl;
+	std :: cout << "\n Elapsed time = " << elapsed_time <<"s \n"<< std:: endl;
+	std :: cout << "\n End time " << ctime(&curr_time) << "\n";
 	std :: cout << "-----------"<<"\n"<< std:: endl;
 
 	//Other final messages like physical time
@@ -201,12 +210,14 @@ if(proc_ID==0){
 
 // _______________ FINAL PRINTS ___________________________
 
-	kmc.print_final(n_steps/print_every);
+kmc.print_final(n_steps/print_every);
 
 MPI_Finalize();
 
 
- return 0;
+
+
+return 0;
  
 }
 
